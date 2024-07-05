219acbbd8278f8d626839f3800f143bfc1456a82
squally13 <aras130203@gmail.com>
2024-07-05 03:21:13 +0200

    Add main menu button functionality

--- Gejm/Assets/GameManager.cs
+++ Gejm/Assets/GameManager.cs
@@ -28,4 +28,9 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+
+    public void mainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
 }
