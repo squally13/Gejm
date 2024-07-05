d0ee93e56d3c3ccf44ea2b6e29534b8478fe0ff2
squally13 <aras130203@gmail.com>
2024-07-05 02:13:13 +0200

    Repaired

--- Gejm/Assets/EnemyWeapon.cs
+++ Gejm/Assets/EnemyWeapon.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_Weapon : MonoBehaviour
+public class EnemyWeapon : MonoBehaviour
 {
     public int attackDamage = 20;
     public int enragedAttackDamage = 40;
